032ff9496e219cef9bf24c8d4b3ff68ef8cf1796
sandeepjoseph <sandeepjoseph4@gmail.com>
2014-10-14 23:48:09 -0400

    initial

---
+++ WebCrawler/filewalk.py
@@ -0,0 +1,18 @@
+import os
+import fnmatch
+import pickle
+
+filepath  = os.getcwd() + '/fortune1'
+list1 = []
+for dirpath, dirs, files in os.walk(filepath):
+    for single_file in files:
+        if (fnmatch.fnmatch(single_file,'*log') or fnmatch.fnmatch(single_file,'*txt')):
+            dirpath1 = dirpath + "\\" + single_file
+            content = open(dirpath1,'r')
+            tuple1 = (dirpath1, content.read())
+            list1.append(tuple1)
+
+var1 = os.path.join(os.getcwd(),'rawdata.pickle')
+write  = open(var1,'wb')
+pickle.dump(list1,write)
+print(list1)
